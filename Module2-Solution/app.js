edb5f7e40583f57985406461971aaae3349a29ca
mFritz <martenfritz@gmail.com>
2016-09-25 21:43:40 +0200

    Added solution for assignment 2 and starter code for assignment 3.

---
+++ Module2-Solution/app.js
@@ -0,0 +1,98 @@
+(function () {
+  'use strict';
+
+  var app =  angular.module('ShoppingListCheckOff', []);
+  app.controller('RemainingListController', RemainingListController);
+  app.controller('BoughtListController', BoughtListController);
+  app.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+  RemainingListController.$inject = ['ShoppingListCheckOffService']
+
+  function RemainingListController (ShoppingListCheckOffService) {
+    var remainingListController = this;
+
+    remainingListController.Items = ShoppingListCheckOffService.GetRemainingItems();
+
+    remainingListController.RemoveItem = function(index){
+      var item = {
+        name: remainingListController.Items[index].name,
+        quantity: remainingListController.Items[index].quantity
+      };
+      ShoppingListCheckOffService.RemoveRemainingItem(item.name, item.quantity);
+      ShoppingListCheckOffService.AddToBoughtItems(item.name, item.quantity);
+    };
+  };
+
+  BoughtListController.$inject = ['ShoppingListCheckOffService']
+
+  function BoughtListController (ShoppingListCheckOffService) {
+    var boughtListController = this;
+    boughtListController.Items = ShoppingListCheckOffService.GetBoughtItems();
+
+  };
+
+  function ShoppingListCheckOffService(){
+    var service = this;
+    var remainingItems = [];
+    var boughtItems = [];
+
+    var initList = function(boughtItems){
+      var item1 = {
+        name: "Chicken",
+        quantity: 2
+      };
+      remainingItems.push(item1);
+
+      var item2 = {
+        name: "Apples",
+        quantity: 5
+      };
+      remainingItems.push(item2);
+
+      var item3 = {
+        name: "Cookies",
+        quantity: 1000
+      };
+      remainingItems.push(item3);
+    }
+
+    initList(remainingItems);
+
+    service.GetRemainingItems = function() {
+      return remainingItems;
+    };
+
+    service.GetBoughtItems = function(){
+      return boughtItems;
+    };
+
+    service.RemoveRemainingItem = function(itemName, itemQuantity) {
+      for (var i = 0; i < remainingItems.length; i++) {
+        var item = remainingItems[i];
+        var itemFound = false;
+        var indexToBeRemoved = -1;
+        if ((item.name == itemName) && (item.quantity == itemQuantity))
+        {
+          itemFound = true;
+          indexToBeRemoved = i;
+          break;
+        }
+      }
+      if (itemFound == false)
+      {
+        throw "item was not found and could not be removed";
+      }
+      remainingItems.splice(indexToBeRemoved, 1);
+    };
+
+    service.AddToBoughtItems = function(itemName, itemQuantity) {
+      var item = {
+        name: itemName,
+        quantity: itemQuantity
+      };
+      boughtItems.push(item);
+    };
+
+  };
+
+})();
